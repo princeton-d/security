7235a34affd1122b80596eae44e6ae616ed0822d
princeton <ssk0080@gmail.com>
2023-06-27 14:08:55 +0900

    init

---
+++ src/test/java/princeton/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package princeton.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
